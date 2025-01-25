1f7fb6af540d7605ff7f10a25ebf0155fa6946a6
Juuso Piippo <158200846+JuJuz1@users.noreply.github.com>
2025-01-25 19:31:16 +0200

    Resource initialization

---
+++ graphics/emojis/negative/emoji_data.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name EmojiData
+
+@export var string: String = ""
+@export var svg: CompressedTexture2D
---
+++ graphics/emojis/negative/emoji_datas.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name EmojiDatas
+
+@export var emojis: Array[EmojiData] = []
+@export var negative_emojis: Array[EmojiData] = []
+@export var positive_emojis: Array[EmojiData] = []
