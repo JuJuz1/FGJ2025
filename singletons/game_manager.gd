ae2114983ee51a37d2ebdd81aa683b46fca7040c
Juuso Piippo <158200846+JuJuz1@users.noreply.github.com>
2025-01-25 22:19:05 +0200

    Reorganize nodes, start menu

--- singletons/game_manager.gd
+++ singletons/game_manager.gd
@@ -1,6 +1,9 @@
 extends Node
 ## Game manager class
 
+## A bool to determine wheter to start the game immediately
+var start_from_start_screen: bool = true
+
 ## Freezes the game
 var freeze: bool = false
 
---
+++ world/start_menu.gd
@@ -0,0 +1,29 @@
+extends Node3D
+## Start menu
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if not GameManager.start_from_start_screen:
+		start_game()
+
+
+func _on_button_start_pressed() -> void:
+	start_game()
+
+
+func _on_button_credits_pressed() -> void:
+	pass # Replace with function body.
+	# TODO
+
+
+func _on_button_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func start_game() -> void:
+	get_tree().change_scene_to_file("res://world/world.tscn")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
